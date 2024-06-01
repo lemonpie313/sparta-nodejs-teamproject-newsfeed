ca1643634d1c60e6f0b8d47857d693d6a7c43a67
t2023-m0091 <t2023-m0091@t2023-m0091ui-MacBookAir.local>
2024-06-01 16:33:28 +0900

    feat: 로그인 기능 구현

--- src/middlewares/joi/auth.joi.middleware.js
+++ src/middlewares/joi/auth.joi.middleware.js
@@ -1,5 +1,6 @@
 import Joi from 'joi';
 import { MESSAGES } from '../../const/messages.const.js';
+import router from '../../routers/auth.router.js';
 
 const signUpSchema = Joi.object({
   email: Joi.string().email().required().messages({
@@ -21,6 +22,17 @@ const signUpSchema = Joi.object({
   }),
 });
 
+const signInSchema = Joi.object({
+  email: Joi.string().email().required().messages({
+    'any.required': MESSAGES.AUTH.COMMON.EMAIL.REQUIRED,
+    'string.email': MESSAGES.AUTH.COMMON.EMAIL.INVALID_FORMAT,
+  }),
+  password: Joi.string().required().min(6).messages({
+    'any.required': MESSAGES.AUTH.COMMON.PASSWORD.REQUIRED,
+    'string.min': MESSAGES.AUTH.COMMON.PASSWORD.MIN_LENGTH,
+  }),
+});
+
 const signUpValidator = async (req, res, next) => {
   try {
     await signUpSchema.validateAsync(req.body);
@@ -30,4 +42,13 @@ const signUpValidator = async (req, res, next) => {
   }
 };
 
-export { signUpValidator };
+const signInValidator = async (req, res, next) => {
+  try {
+    await signInSchema.validateAsync(req.body);
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
+
+export { signUpValidator, signInValidator };
--- src/routers/auth.router.js
+++ src/routers/auth.router.js
@@ -1,10 +1,14 @@
 import express from 'express';
 import { prisma } from '../utils/prisma/index.js';
-import { signUpValidator } from '../middlewares/joi/auth.joi.middleware.js';
+import {
+  signUpValidator,
+  signInValidator,
+} from '../middlewares/joi/auth.joi.middleware.js';
 import { MESSAGES } from '../const/messages.const.js';
 import { HTTP_STATUS } from '../const/http-status.const.js';
 import bycrpt from 'bcrypt';
 import { Prisma } from '@prisma/client';
+import jwt from 'jsonwebtoken';
 
 const router = express.Router();
 
@@ -71,5 +75,75 @@ router.post('/sign-up', signUpValidator, async (req, res, next) => {
     next(err);
   }
 });
+
+//로그인
+router.post('/sign-in', signInValidator, async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+
+    //회원정보 가져오기
+    const user = await prisma.Users.findFirst({
+      where: {
+        email,
+      },
+      select: {
+        userId: true,
+        password: true,
+      },
+    });
+    if (!user) {
+      return res.status(HTTP_STATUS.NOT_FOUND).json({
+        status: HTTP_STATUS.NOT_FOUND,
+        message: MESSAGES.AUTH.SIGN_IN.IS_NOT_EXIST,
+      });
+    }
+
+    //입력받은것, 저장되어있는것
+    if (!(await bycrpt.compare(password, user.password))) {
+      return res.status(HTTP_STATUS.BAD_REQUEST).json({
+        status: HTTP_STATUS.BAD_REQUEST,
+        message: MESSAGES.AUTH.SIGN_IN.PW_NOT_MATCHED,
+      });
+    }
+
+    const payload = { id: user.userId };
+    const data = await token(payload);
+
+    res.status(HTTP_STATUS.OK).json({
+      status: HTTP_STATUS.OK,
+      message: MESSAGES.AUTH.SIGN_IN.SUCCEED,
+      data,
+    });
+  } catch (err) {
+    next(err);
+  }
+});
+
+const token = async function (payload) {
+  const userId = payload.id;
+  const accessToken = jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET_KEY, {
+    expiresIn: '12h',
+  });
+  const refreshToken = jwt.sign(payload, process.env.REFRESH_TOKEN_SECRET_KEY, {
+    expiresIn: '7d',
+  });
+
+  const refreshTokenHashed = await bycrpt.hash(refreshToken, 10);
+
+  await prisma.RefreshToken.upsert({
+    where: {
+      userId,
+    },
+    update: {
+      token: refreshTokenHashed,
+    },
+    create: {
+      userId,
+      token: refreshTokenHashed,
+    },
+  });
+
+  return { accessToken, refreshToken };
+};
 
 export default router;
