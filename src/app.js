ba75e2ecb497a22f79aac1724814b66cac132ee6
CDR4733 <shsing2@naver.com>
2024-06-02 13:43:35 +0900

    dfdf w

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import dotEnv from 'dotenv';
 import authRouter from './routers/auth.router.js';
 import usersRouter from './routers/users.router.js';
 import postsRouter from './routers/posts.router.js';
-// import commentsRouter from './routers/comments.router.js';
+import commentsRouter from './routers/comments.router.js';
 import errorHandler from './middlewares/error-handler.middleware.js';
 
 dotEnv.config();
@@ -13,16 +13,16 @@ const PORT = 3000; // 서버를 열 때 사용할 포트 번호
 app.use(express.json());
 
 app.get('/', (req, res) => {
-	res.send('루트!!');
+  res.send('루트!!');
 });
 
 app.use('/auth', [authRouter]);
 app.use('/users', [usersRouter]);
 app.use('/posts', [postsRouter]);
-// app.use('/comments', [commentsRouter]);
+app.use('/comments', [commentsRouter]);
 
 app.use(errorHandler);
 
 app.listen(PORT, () => {
-	console.log(PORT, '포트로 서버가 열렸어요!');
+  console.log(PORT, '포트로 서버가 열렸어요!');
 });
