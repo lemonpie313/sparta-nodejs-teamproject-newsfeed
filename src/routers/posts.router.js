9a5ddc56e9dda2b4f8ed90b9b77fc57abb123a10
jiyoon-na <164995957+jiyoon-na@users.noreply.github.com>
2024-06-02 14:29:21 +0900

    feat 9-04개발-게시물-삭제

--- src/routers/posts.router.js
+++ src/routers/posts.router.js
@@ -3,91 +3,125 @@ import { prisma } from '../utils/prisma/index.js';
 import authMiddleware from '../middlewares/access-token.middleware.js';
 import { MESSAGES } from '../const/messages.const.js';
 import { HTTP_STATUS } from '../const/http-status.const.js';
+import { symbol } from 'joi';
 
 const router = express.Router();
 
 //게시물 작성
 router.post('/:group', authMiddleware, async (req, res, next) => {
-	try {
-		// 1. 필요한 정보 가져오기
-		// 1-1. request body로부터 postContent, postPicture, keywords 받아온다.
-		const { postContent, postPicture, keywords } = req.body;
-		// 1-2. group 가져오기
-		const { group } = req.params;
+  try {
+    // 1. 필요한 정보 가져오기
+    // 1-1. request body로부터 postContent, postPicture, keywords 받아온다.
+    const { postContent, postPicture, keywords } = req.body;
+    // 1-2. group 가져오기
+    const { group } = req.params;
 
-		// 2. 만약에 셋 중 하나라도 없으면 에러 처리
-		if (!postContent) {
-			return res.status(HTTP_STATUS.BAD_REQUEST).json({
-				status: HTTP_STATUS.BAD_REQUEST,
-				message: MESSAGES.POSTS.CREATE.NO_POSTCONTENT,
-			});
-		}
-		if (!postPicture) {
-			return res.status(HTTP_STATUS.BAD_REQUEST).json({
-				status: HTTP_STATUS.BAD_REQUEST,
-				message: MESSAGES.POSTS.CREATE.NO_POSTPICTURE,
-			});
-		}
-		if (!keywords) {
-			return res.status(HTTP_STATUS.BAD_REQUEST).json({
-				status: HTTP_STATUS.BAD_REQUEST,
-				message: MESSAGES.POSTS.CREATE.NO_KEYWORDS,
-			});
-		}
+    // 2. 만약에 셋 중 하나라도 없으면 에러 처리
+    if (!postContent) {
+      return res.status(HTTP_STATUS.BAD_REQUEST).json({
+        status: HTTP_STATUS.BAD_REQUEST,
+        message: MESSAGES.POSTS.CREATE.NO_POSTCONTENT,
+      });
+    }
+    if (!postPicture) {
+      return res.status(HTTP_STATUS.BAD_REQUEST).json({
+        status: HTTP_STATUS.BAD_REQUEST,
+        message: MESSAGES.POSTS.CREATE.NO_POSTPICTURE,
+      });
+    }
+    if (!keywords) {
+      return res.status(HTTP_STATUS.BAD_REQUEST).json({
+        status: HTTP_STATUS.BAD_REQUEST,
+        message: MESSAGES.POSTS.CREATE.NO_KEYWORDS,
+      });
+    }
 
-		// 3. 작성자 유저 ID를 req.user로 받아오고,
-		const { UserId } = req.user;
-		// 4. 작성한 내용을 바탕으로 posts 테이블에 생성
-		const post = await prisma.posts.create({
-			data: {
-				group,
-				UserId: +UserId,
-				postContent,
-				postPicture,
-				keywords,
-			}
-		});
-		// 5. 생성 결과를 클라이언트에 반환
-		// 5-1. post_id, User_id, nickname, post_content, post_picture, keywords
+    // 3. 작성자 유저 ID를 req.user로 받아오고,
+    const { UserId } = req.user;
+    // 4. 작성한 내용을 바탕으로 posts 테이블에 생성
+    const post = await prisma.posts.create({
+      data: {
+        group,
+        UserId: +UserId,
+        postContent,
+        postPicture,
+        keywords,
+      },
+    });
+    // 5. 생성 결과를 클라이언트에 반환
+    // 5-1. post_id, User_id, nickname, post_content, post_picture, keywords
 
-		return res.status(HTTP_STATUS.CREATED).json({
-			status: HTTP_STATUS.CREATED,
-			message: MESSAGES.POSTS.CREATE.SUCCEED,
-			data: post,
-		})
-	} catch (err) {
-		next(err);
-	}
+    return res.status(HTTP_STATUS.CREATED).json({
+      status: HTTP_STATUS.CREATED,
+      message: MESSAGES.POSTS.CREATE.SUCCEED,
+      data: post,
+    });
+  } catch (err) {
+    next(err);
+  }
 });
 
 // 내 게시물 목록 조회
 router.get('/me', authMiddleware, async (req, res, next) => {
-	try {
-		// 1. 받아온 req.user에서 userId 가져온다.
-		const { UserId } = req.user;
+  try {
+    // 1. 받아온 req.user에서 userId 가져온다.
+    const { UserId } = req.user;
 
-		// 2. 로그인한 사용자의 게시물 목록만 조회한다.
-		const posts = await prisma.posts.findMany({
-			where: {
-				UserId,
-			},
-			orderBy: {
-				createdAt: "desc"
-			}
-		})
+    // 2. 로그인한 사용자의 게시물 목록만 조회한다.
+    const posts = await prisma.posts.findMany({
+      where: {
+        UserId,
+      },
+      orderBy: {
+        createdAt: 'desc',
+      },
+    });
 
-
-		// 3. 결과 반환한다.
-		return res.status(HTTP_STATUS.OK).json({
-			status: HTTP_STATUS.OK,
-			message: MESSAGES.POSTS.READ.SUCCEED,
-			data: posts,
-		})
-
-	} catch (err) {
-		next(err);
-	}
+    // 3. 결과 반환한다.
+    return res.status(HTTP_STATUS.OK).json({
+      status: HTTP_STATUS.OK,
+      message: MESSAGES.POSTS.READ.SUCCEED,
+      data: posts,
+    });
+  } catch (err) {
+    next(err);
+  }
 });
 
+//게시물 삭제
+router.delete('/:postId', authMiddleware, async (req, res, next) => {
+  try {
+    //유저 아이디를 req.user에서 가져옴
+    const { UserId } = req.user;
+
+    //포스트 아이디를 req.params에서 가져옴
+    const { postId } = req.params;
+
+    //해당 게시물이 존재 하는지 검사
+    if (!postId) {
+      return res.status(HTTP_STATUS.NOT_FOUND).json({
+        message: MESSAGES.POSTS.DELETE.NO_POSTID,
+      });
+    }
+    //로그인한 유저id와 게시물 작성한 유저id가 같은지 확인
+    if (UserId !== posts.UserId) {
+      return res.status(HTTP_STATUS.ERROR).json({
+        message: MESSAGES.POSTS.DELETE.POST_ID_NOT_MATCHED,
+      });
+    }
+    //삭제구현
+    const Post = await prisma.posts.delete({
+      where: { postId: +postId },
+    });
+
+    return res.status(HTTP_STATUS.OK).json({
+      status: HTTP_STATUS.OK,
+      message: MESSAGES.POSTS.DELETE.SUCCEED,
+      post: { postId: postId },
+    });
+  } catch (err) {
+    next(err);
+  }
+});
 
 export default router;
