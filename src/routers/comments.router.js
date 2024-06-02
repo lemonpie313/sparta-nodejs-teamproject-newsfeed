d456672060ed3f033c326bdc8f96118b096ac5f0
CDR4733 <shsing2@naver.com>
2024-06-02 14:08:53 +0900

    feat : code conflict 해결

--- src/routers/comments.router.js
+++ src/routers/comments.router.js
@@ -6,85 +6,62 @@ import { MESSAGES } from '../const/messages.const.js';
 
 const router = express.Router();
 
-//게시물 작성
-router.post('/:group', authMiddleware, async (req, res, next) => {
+/** 댓글 작성 API **/
+router.post('/:postId', authMiddleware, async (req, res, next) => {
   try {
-    // 1. 필요한 정보 가져오기
-    // 1-1. request body로부터 postContent, postPicture, keywords 받아온다.
-    const { postContent, postPicture, keywords } = req.body;
-    // 1-2. group 가져오기
-    const { group } = req.params;
-
-    // 2. 만약에 셋 중 하나라도 없으면 에러 처리
-    if (!postContent) {
-      return res.status(HTTP_STATUS.BAD_REQUEST).json({
-        status: HTTP_STATUS.BAD_REQUEST,
-        message: MESSAGES.POSTS.CREATE.NO_POSTCONTENT,
-      });
-    }
-    if (!postPicture) {
-      return res.status(HTTP_STATUS.BAD_REQUEST).json({
-        status: HTTP_STATUS.BAD_REQUEST,
-        message: MESSAGES.POSTS.CREATE.NO_POSTPICTURE,
-      });
-    }
-    if (!keywords) {
-      return res.status(HTTP_STATUS.BAD_REQUEST).json({
-        status: HTTP_STATUS.BAD_REQUEST,
-        message: MESSAGES.POSTS.CREATE.NO_KEYWORDS,
-      });
-    }
-
-    // 3. 작성자 유저 ID를 req.user로 받아오고,
+    // 1. 댓글 작성에 필요한 정보 가져오기
+    // 1-1. req.body로부터 comment를 받아온다.
+    const { comment } = req.body;
+    // 1-2. req.params로부터 postId 받아온다.
+    const { postId } = req.params;
+    console.log(postId);
+    // 1-3. req.user로부터 UserId 가져온다.
     const { UserId } = req.user;
-    // 4. 작성한 내용을 바탕으로 posts 테이블에 생성
-    const post = await prisma.posts.create({
+    // 2. 만약 댓글을 입력하지 않았다면
+    if (!comment) {
+      return res.status(HTTP_STATUS.BAD_REQUEST).json({
+        status: HTTP_STATUS.BAD_REQUEST,
+        message: MESSAGES.COMMENTS.CREATE.NO_COMMENTS,
+      });
+    }
+    // 3. 작성한 내용을 바탕으로 comments 테이블에 comment 생성
+    const newComment = await prisma.comments.create({
+      //comment는 위에서 선언해서.. newComment로 선언함
       data: {
-        group,
+        PostId: +postId,
         UserId: +UserId,
-        postContent,
-        postPicture,
-        keywords,
+        comment,
       },
     });
-    // 5. 생성 결과를 클라이언트에 반환
-    // 5-1. post_id, User_id, nickname, post_content, post_picture, keywords
-
+    // 4. 댓글 작성 결과를 클라이언트에 반환
     return res.status(HTTP_STATUS.CREATED).json({
       status: HTTP_STATUS.CREATED,
-      message: MESSAGES.POSTS.CREATE.SUCCEED,
-      data: post,
+      message: MESSAGES.COMMENTS.CREATE.SUCCEED,
+      data: newComment,
     });
   } catch (err) {
     next(err);
   }
 });
 
-// 내 게시물 목록 조회
-router.get('/me', authMiddleware, async (req, res, next) => {
+/** 댓글 삭제 API **/
+router.delete('/:commentId', authMiddleware, async (req, res, next) => {
   try {
-    // 1. 받아온 req.user에서 userId 가져온다.
+    // 1. 댓글 작성에 필요한 정보 가져오기
+    // 1-1. req.params로부터 commentId 받아온다.
+    const { commentId } = req.params;
+    // 1-2. req.user로부터 UserId 가져온다.
     const { UserId } = req.user;
-
-    // 2. 로그인한 사용자의 게시물 목록만 조회한다.
-    const posts = await prisma.posts.findMany({
+    // 2. 해당 댓글이 존재하는지 확인
+    const comment = await prisma.comments.findFirst({
       where: {
-        UserId,
-      },
-      orderBy: {
-        createdAt: 'desc',
+        UserId: +UserId,
       },
     });
 
-    // 3. 결과 반환한다.
-    return res.status(HTTP_STATUS.OK).json({
-      status: HTTP_STATUS.OK,
-      message: MESSAGES.POSTS.READ.SUCCEED,
-      data: posts,
-    });
+    // 4. 댓글 작성 결과를 클라이언트에 반환
+    return res.status(HTTP_STATUS.CREATED).json({});
   } catch (err) {
     next(err);
   }
 });
-
-export default router;
