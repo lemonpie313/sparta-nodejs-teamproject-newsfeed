82d53b8caa03eac4636a138225da1c13dc87c605
SuhyeonJin <lofi0613@gmail.com>
2024-06-04 16:52:59 +0900

    feat: 아티스트 회원가입 구현

--- src/routers/auth.router.js
+++ src/routers/auth.router.js
@@ -2,6 +2,7 @@ import express from 'express';
 import { prisma } from '../utils/prisma/index.js';
 import {
   signUpValidator,
+  signUpArtistValidator,
   signInValidator,
 } from '../middlewares/joi/auth.joi.middleware.js';
 import { MESSAGES } from '../const/messages.const.js';
@@ -11,10 +12,11 @@ import { Prisma } from '@prisma/client';
 import jwt from 'jsonwebtoken';
 import { ROLE } from '../const/role.const.js';
 import refreshTokenMiddleware from '../middlewares/refresh-token.middleware.js';
+import { ARTIST_ID } from '../const/artistId.const.js';
 
 const router = express.Router();
 
-//회원가입 - 리팩토링 완료
+//회원가입 - 팬 전용
 router.post('/sign-up', signUpValidator, async (req, res, next) => {
   try {
     const {
@@ -58,6 +60,100 @@ router.post('/sign-up', signUpValidator, async (req, res, next) => {
             name,
             nickname,
             role: ROLE.FAN,
+            selfIntroduction,
+            profilePicture: profilePicture ?? 'image.jpg',
+          },
+          select: {
+            name: true,
+            role: true,
+            nickname: true,
+            selfIntroduction: true,
+            profilePicture: true,
+            createdAt: true,
+            updatedAt: true,
+          },
+        });
+        return { ...user, ...userInfo };
+      },
+      {
+        isolationLevel: Prisma.TransactionIsolationLevel.ReadCommitted,
+      }
+    );
+
+    return res.status(HTTP_STATUS.CREATED).json({
+      status: HTTP_STATUS.CREATED,
+      message: MESSAGES.AUTH.SIGN_UP.SUCCEED,
+      data: { userInfo },
+    });
+  } catch (err) {
+    next(err);
+  }
+});
+
+//회원가입 - 아티스트 전용
+router.post('/sign-up/artists', signUpArtistValidator, async (req, res, next) => {
+  try {
+    const {
+      email,
+      artistId,
+      password,
+      name,
+      nickname,
+      selfIntroduction,
+      profilePicture,
+    } = req.body;
+    const isExistEmail = await prisma.users.findFirst({
+      //db의 이메일:body의 이메일
+      where: {
+        email,
+      },
+    });
+    if (isExistEmail) {
+      return res.status(HTTP_STATUS.CONFLICT).json({
+        status: HTTP_STATUS.CONFLICT,
+        message: MESSAGES.AUTH.SIGN_UP.NOT_AVAILABLE,
+      });
+    }
+
+    let artist;
+    switch (artistId) {
+      case '0000':
+        artist = ROLE.MONSTAX;
+        break;
+      case '1111':
+        artist = ROLE.WJSN;
+        break;
+      case '2222':
+        artist = ROLE.CRAVITY;
+        break;
+      case '3333':
+        artist = ROLE.IVE;
+      default:
+        return res.status(HTTP_STATUS.FORBIDDEN).json({
+          status: HTTP_STATUS.FORBIDDEN,
+          message: MESSAGES.AUTH.SIGN_UP_ARTIST.NOT_AVAILABLE,
+        });
+      }
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const userInfo = await prisma.$transaction(
+      async (tx) => {
+        const user = await tx.Users.create({
+          data: {
+            email,
+            password: hashedPassword,
+          },
+          select: {
+            userId: true,
+            email: true,
+          },
+        });
+        const userInfo = await tx.UserInfos.create({
+          data: {
+            UserId: user.userId,
+            name,
+            nickname,
+            role: artist,
             selfIntroduction,
             profilePicture: profilePicture ?? 'image.jpg',
           },
